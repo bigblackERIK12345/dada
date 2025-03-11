fa59a74309ff56324294c2fd45eca03e454f978f
bigblackERIK12345 <tanjirotranjiro@gmail.com>
2025-03-11 18:30:27 +0300

    Первый коммит

---
+++ src/lib/componets/Header.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+    import {Nav,Navbar, NavbarBrand,NavItem,NavLink} from  '@sveltestrap/sveltestrap';
+</script>
+<Navbar>
+<NavbarBrand>Logo</NavbarBrand>
+
+<Nav>
+    <NavItem><NavLink>Основы упражнений</NavLink></NavItem>
+    <NavItem><NavLink>Профилактика здоровья</NavLink></NavItem>
+    <NavItem><NavLink>Вредная еда</NavLink></NavItem>
+</Nav>
+</Navbar>
---
+++ src/routes/+layout.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+    import Header from "$lib/componets/Header.svelte"
+    import {Container,Styles}from "@sveltestrap/sveltestrap"
+ let {children} = $props();
+</script>
+
+<Styles/>
+<Container>
+    <Header/>
+    {@render children()}
+</Container>
