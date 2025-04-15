788521519138cff4d5beeaf0a8cbc575fe293bf9
stud <stud@it233-13.almetpt.local>
2025-04-15 18:29:03 +0300

    jerking

--- src/routes/Doggy/+page.svelte
+++ src/routes/Doggy/+page.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
      import { GetToDos,Todos } from "$lib/store/ToDoStore";
-    import { CardBody, Container,Card } from "@sveltestrap/sveltestrap";
+    import { CardBody, Container, Card, Row, Col} from "@sveltestrap/sveltestrap";
  import { onMount } from "svelte";
  onMount(()=>{
     async function FetchTodos() {
@@ -14,10 +14,13 @@
 </svelte:head>
 
 <Container>
+    <Row>
  {#each $Todos as todo}
  <Col md={2}>
  <Card>
     <CardBody>{todo.title}</CardBody>
  </Card>
+</Col>
 {/each}
+</Row>
 </Container>
