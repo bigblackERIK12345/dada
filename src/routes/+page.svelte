f8060464b050715bcd7b82ea46db993dde8b3f0e
stud <stud@it233-13.almetpt.local>
2025-04-29 19:02:46 +0300

    квадрат

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,43 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+    import { CardBody, CardHeader, CardImg, Col, Row, Card, CardText, Container, } from "@sveltestrap/sveltestrap";
+
+
+</script>
+
+<svelte:head>
+    <title>О нас</title>
+</svelte:head>
+
+<Container>
+    <Row>
+        <Col>
+            <Card>
+                <CardHeader>Участники</CardHeader>
+                <CardBody>
+                    <CardImg src="">
+                        <CardText></CardText>
+                </CardBody>
+            </Card>
+        </Col>
+    </Row>
+
+    <Col>
+        <Card>
+            <CardHeader>Командир</CardHeader>
+            <CardBody>
+                <CardImg src="https://arbuztoday.ru/wp-content/uploads/2023/08/width.jpeg">
+                    <CardText></CardText>
+            </CardBody>
+        </Card>
+    </Col>
+
+    <Col>
+        <Card>
+            <CardHeader>Символ</CardHeader>
+            <CardBody>
+                <CardImg src="https://avatars.mds.yandex.net/get-yapic/45848/enc-38cd8d84028180e1f374eae8b5da6460b4f074f2dc68035960352ba58fdfaef5/orig">
+                    <CardText></CardText>
+            </CardBody>
+        </Card>
+    </Col>
+</Container>
